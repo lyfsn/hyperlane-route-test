94a53025eeabee6b28c466776e5bd4f7fd46f4d6
lyfsn <dev.wangyu@proton.me>
2024-06-28 15:01:08 +0800

    up

--- src/consts/chains.ts
+++ src/consts/chains.ts
@@ -52,29 +52,6 @@ export const chains: ChainMap<ChainMetadata & { mailbox?: Address }> = {
     },
     // logoURI: '/logo.svg',
   },
-  devnetc: {
-    protocol: ProtocolType.Ethereum,
-    chainId: 64646464,
-    domainId: 64646464,
-    name: 'devneta',
-    displayName: 'devneta',
-    nativeToken: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-    rpcUrls: [{ http: 'https://rpc-devnet-a.archivenode.club' }],
-    blockExplorers: [
-      // {
-      //   name: 'MyCustomScan',
-      //   url: 'https://mycustomchain-scan.com',
-      //   apiUrl: 'https://api.mycustomchain-scan.com/api',
-      //   family: "etherscan",
-      // },
-    ],
-    blocks: {
-      confirmations: 1,
-      reorgPeriod: 1,
-      estimateBlockTime: 10,
-    },
-    // logoURI: '/logo.svg',
-  },
   devnetb: {
     protocol: ProtocolType.Ethereum,
     chainId: 74747474,
